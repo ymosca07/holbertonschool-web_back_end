25ee62959853b741f4dea7f8992f66ada8972740
Yannis <yannismosca@gmail.com>
2025-03-25 10:11:26 +0100

    Push

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,6 +1,7 @@
 export default function taskBlock() {
-    const task = false;
-    const task2 = true;
   
-    return [task, task2];
-  }
+  const task = false;
+  const task2 = true;
+  
+  return [task, task2];
+}
