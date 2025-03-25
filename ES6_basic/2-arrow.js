95a409c95dd111e3f8b6646a1e243e6e1eb7c011
Yannis <yannismosca@gmail.com>
2025-03-25 11:08:35 +0100

    Push

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,4 +1,6 @@
-this.addNeighborhood = (newNeighborhood) => {
-  this.sanFranciscoNeighborhoods.push(newNeighborhood);
-  return this.sanFranciscoNeighborhoods;
-};
+export default function getNeighborhoodsList() {
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
