9c1deda95b30045e4345af2b0e118e76accd176a
Yannis <yannismosca@gmail.com>
2025-04-01 11:59:52 +0200

    Push

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -2,5 +2,5 @@ export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw new Error('cannot divide by 0');
   }
-    return numerator / denominator;
+  return numerator / denominator;
 }
