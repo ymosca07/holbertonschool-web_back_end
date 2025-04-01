d65ae32ccece9461f3f38c45a76dd491cbe26b9a
Yannis <yannismosca@gmail.com>
2025-04-01 08:48:04 +0200

    Push

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-    return students.map((student) => student.id);
+  if (!Array.isArray(students)) {
+    return [];
   }
+  return students.map((student) => student.id);
+}
