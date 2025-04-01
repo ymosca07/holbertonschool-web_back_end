4abe1fe39ea1561f2c2c3d87447736390f599f59
Yannis <yannismosca@gmail.com>
2025-04-01 08:39:47 +0200

    Push

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
 export default function getListStudents() {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' },
-      ];
+  const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  const list = [student1, student2, student3];
+
+  return list;
 }
