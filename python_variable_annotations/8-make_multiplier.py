652e504d78cc6e395a1e3500a69466aa561491d2
Yannis <yannismosca@gmail.com>
2025-04-23 14:54:42 +0200

    Push

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 """Ceci est une description"""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Ceci est une description"""
     return lambda x: x * multiplier
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """Ceci est une description"""
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Ceci est une description"""
     return [(i, len(i)) for i in lst]
