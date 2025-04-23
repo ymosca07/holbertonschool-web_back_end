3f1e6444b2bf534f91db754682ae1080f494c132
Yannis <yannismosca@gmail.com>
2025-04-23 14:26:07 +0200

    Push

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Ceci est une description"""
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     return k, v*v
