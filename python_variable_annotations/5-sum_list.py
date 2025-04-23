5cc498b18de405a0819ec8f5f3fa4242a3f409ce
Yannis <yannismosca@gmail.com>
2025-04-23 14:09:36 +0200

    Push

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Ceci est une description"""
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: list[float]) -> float:
     """Ceci est une description"""
     return sum(input_list)
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Ceci est une description"""
+from typing import List
 
 
-def sum_mixed_list(mxd_lst: list) -> float:
+def sum_mixed_list(mxd_lst: list[int, float]) -> float:
     """Ceci est une description"""
+    return sum(mxd_lst)
