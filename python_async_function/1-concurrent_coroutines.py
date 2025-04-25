1b89c96785e21b276983f03575f4bdf0994b7888
Yannis <yannismosca@gmail.com>
2025-04-25 10:40:57 +0200

    Push

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Ceci est une description"""
     delays = []
-    for task in asyncio.as_completed([wait_random(max_delay) for _ in range(n)]):
+    for task in asyncio.as_completed([wait_random(max_delay)
+                                      for _ in range(n)]):
         delay = await task
         delays.append(delay)
     return delays
