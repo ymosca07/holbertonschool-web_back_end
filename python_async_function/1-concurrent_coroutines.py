0803827360e552d8c902214f9c58ac3a6ce1b98b
Yannis <yannismosca@gmail.com>
2025-04-25 10:39:55 +0200

    Push

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Ceci est une description"""
-
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Ceci est une description"""
