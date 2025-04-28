1b802b0aedc4d396510e57f5ec089df09af16bc0
Yannis <yannismosca@gmail.com>
2025-04-28 11:14:44 +0200

    Push

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,14 +8,14 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Ceci est une description"""
     start_time = time.perf_counter()
-    
+
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    
+
     end_time = time.perf_counter()
-    
+
     return end_time - start_time
