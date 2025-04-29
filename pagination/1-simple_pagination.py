e367f4783d6a05349ec0003d784dd5458bd1acf6
Yannis <yannismosca@gmail.com>
2025-04-29 09:51:00 +0200

    Push

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Ceci est une description"""
+from typing import Tuple
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Ceci est une description"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """This is get page"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+
+        dataset = self.dataset()
+
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
