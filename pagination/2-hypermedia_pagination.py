d0d14f707eb3c4eb3edba6288dfe789bb64263e0
Yannis <yannismosca@gmail.com>
2025-04-29 10:10:41 +0200

    Push

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -44,3 +44,18 @@ class Server:
         if start >= len(dataset):
             return []
         return dataset[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict[str, any]:
+        """This is get hyper"""
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
